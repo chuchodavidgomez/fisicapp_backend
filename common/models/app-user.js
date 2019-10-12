f47fb551d0a13ea6467613c56b82cb48ead19f07
chuchodavidgomez <jesus.gomezo@udea.edu.co>
2019-10-12 10:15:53 -0500

    Se agrego el metodo whoiam

--- common/models/app-user.js
+++ common/models/app-user.js
@@ -1,4 +1,30 @@
 'use strict';
 
 module.exports = function (Appuser) {
+
+    Appuser.whoami = function (callback) {
+        callback();
+    }
+
+    Appuser.afterRemote('whoami', function (context, model, next) {
+        var userId = context.req.accessToken.userId;
+        if (!userId) {
+            return next(getAuthenticationError());
+        }
+        Person.findOne({ where: { id: userId } }, function (error, item) {
+            if (error) {
+                return next(error);
+            }
+            if (!item) {
+                const err = new Error();
+                err.statusCode = 401;
+                err.message = 'Authorization Required';
+                err.code = 'AUTHORIZATION_REQUIRED';
+                return next(err);
+            }
+            context.result = item;
+            next();
+        })
+    });
+
 };
